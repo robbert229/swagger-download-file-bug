a57347e54b7733d7d89cf5b823d491ed2341c04e
John Rowley <John_Rowley@selinc.com>
2017-02-22 11:44:01 -0800

    recreating issue

---
+++ restapi/doc.go
@@ -0,0 +1,25 @@
+/*Package restapi Download Test
+
+download
+
+
+    Schemes:
+      http
+      https
+    Host: api.uber.com
+    BasePath: /
+    Version: 1.0.0
+
+    Consumes:
+    - multipart/form-data
+
+
+    Produces:
+    - application/json
+
+    - application/octet-stream
+
+
+swagger:meta
+*/
+package restapi
